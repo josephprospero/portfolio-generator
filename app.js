066426fb0f4c5d1ed969fb0accb3a1edb45c5443
Joseph Prospero <jprospero413@gmail.com>
2021-02-10 17:27:52 -0800

    set up inquirer

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
+console.log(inquirer)
 
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name:'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+
+/*const fs = require('fs');
 const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
+const pageHTML = generatepage(name, github);
 
 fs.writeFile('index.html', generatePage(name, github), err => {
     if (err) throw err;
 
     console.log('Portfolio complete! Check out index/html to see the output!')
 });
+*/
+
