00b9de9da564a755fb029500f20e4d95d7bc95ec
Joseph Prospero <jprospero413@gmail.com>
2021-02-12 19:13:32 -0800

    added promises and finished site

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const inquirer = require('inquirer');
 const Choice = require('inquirer/lib/objects/choice');
-const fs = require('fs');
-const generatePage = require('./src/page-template')
+const generatePage = require('./src/page-template');
+const generateSite = require('./utils/generate-site.js');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 
 
 inquirer
@@ -136,13 +137,19 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        const pageHTML = generatePage(portfolioData);
-
-        fs.writeFile('./index.html', pageHTML, err => {
-          if (err) throw new Error(err);
-
-          console.log('Page created! Check out index.html in this directory to see it!');
-        });
+        return generatePage(portfolioData);        
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
     });
-
 
